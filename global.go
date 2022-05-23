ae03e6d8cb154ddf625ca9a9b20bbac36e1fc2fb
galaxy-iot <wh8199@foxmain.com>
2022-05-23 17:41:30 +0800

    rename logger to globalLogger

--- global.go
+++ global.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	logger LoggingInterface
+	globalLogger LoggingInterface
 )
 
 func globalLogFormatter(name string, level LoggingLevel, callerLevel int, pool *BufferPool, format string, args ...interface{}) *bytes.Buffer {
@@ -43,61 +43,61 @@ func globalLogFormatter(name string, level LoggingLevel, callerLevel int, pool *
 }
 
 func SetLogLevel(level LoggingLevel) {
-	logger.SetLevel(level)
+	globalLogger.SetLevel(level)
 }
 
 func Debug(args ...interface{}) {
-	logger.Debug(args...)
+	globalLogger.Debug(args...)
 }
 
 func Debugf(format string, args ...interface{}) {
-	logger.Debugf(format, args...)
+	globalLogger.Debugf(format, args...)
 }
 
 func Info(args ...interface{}) {
-	logger.Info(args...)
+	globalLogger.Info(args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	logger.Infof(format, args...)
+	globalLogger.Infof(format, args...)
 }
 
 func Warn(args ...interface{}) {
-	logger.Warn(args...)
+	globalLogger.Warn(args...)
 }
 
 func Warnf(format string, args ...interface{}) {
-	logger.Warnf(format, args...)
+	globalLogger.Warnf(format, args...)
 }
 
 func Error(args ...interface{}) {
-	logger.Error(args...)
+	globalLogger.Error(args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	logger.Errorf(format, args...)
+	globalLogger.Errorf(format, args...)
 }
 
 func Fatal(args ...interface{}) {
-	logger.Fatal(args...)
+	globalLogger.Fatal(args...)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	logger.Fatalf(format, args...)
+	globalLogger.Fatalf(format, args...)
 }
 
 func Println(args ...interface{}) {
-	logger.Info(args...)
+	globalLogger.Info(args...)
 }
 
 func Printf(format string, args ...interface{}) {
-	logger.Infof(format, args...)
+	globalLogger.Infof(format, args...)
 }
 
 func Fatalln(args ...interface{}) {
-	logger.Fatal(args...)
+	globalLogger.Fatal(args...)
 }
 
 func SetOutPut(w io.Writer) {
-	logger.SetOutPut(w)
+	globalLogger.SetOutput(w)
 }
